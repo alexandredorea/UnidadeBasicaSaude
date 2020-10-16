000df30cdfcabb9e7335bc507af25d9d26bdf30c
Alexandre Dórea <alexandre.dorea@gmail.com>
2020-10-16 19:31:54 -0300

    AD:UBS-004 - Refatoração
    Alterado o nome do assembly dos testes; atualizado documentação

--- test/Modelo.Usb.Teste/Integracoes/Servicos/UnidadeBasicaServicoTeste.cs
+++ test/Modelo.Usb.Teste/Integracoes/Servicos/UnidadeBasicaServicoTeste.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Modelo.Ubs.Teste.Integracoes.Servicos
 {
-    public class UnidadeBasicaServicoTeste : AMcomUbsTesteBase
+    public class UnidadeBasicaServicoTeste : ModeloTesteBase
     {
         private IUnidadeSaudeServico servico;
 
@@ -25,11 +25,11 @@ namespace Modelo.Ubs.Teste.Integracoes.Servicos
         [Test]
         public async Task deve_obter_as_unidades_basicas_de_saude_ordernada_por_melhor_avaliacao_e_proximo_das_coordenadas_passadas()
         {
-            //arrange
+            //arrange: ao preparar um dado
             var contrato = new UnidadeSaudeContratoBuilder().Construir();
-            //act
+            //act....: no ato de executar
             var retorno = await servico.ObterUnidadesSaude(contrato).ConfigureAwait(false);
-            //assert
+            //assert.: [posso] afirmar que o resultado esperado
             retorno.Should().HaveCountLessOrEqualTo(5);
             retorno.FirstOrDefault().Nome.Should().NotBeNullOrEmpty();
             retorno.FirstOrDefault().Endereco.Should().NotBeNullOrEmpty();
@@ -58,28 +58,22 @@ namespace Modelo.Ubs.Teste.Integracoes.Servicos
         [TestCase(10.5487, -25234567)]
         public void deve_lancar_excecao_coordenadas_passadas_erradas(double latitude, double longitude)
         {
-            //arrange
             var contrato = new UnidadeSaudeContratoBuilder()
                 .ComLatitude(latitude)
                 .ComLatitude(longitude)
                 .Construir();
-            //act
+
             Func<Task> retorno = async () => await servico.ObterUnidadesSaude(contrato).ConfigureAwait(false);
-            //assert
+
             retorno.Should().Throws<UnidadeSaudeException>();
         }
 
         [Test]
         public void deve_lancar_excecao_se_latitude_for_maior_que_90_graus_positivo_ou_negativo()
         {
-            //arrange
             var contrato = new UnidadeSaudeContratoBuilder()
                 .ComLatitude(91.01)
                 .Construir();
-            //act
-            //Func<Task> retorno = async () => await servico.ObterUnidadesSaude(contrato).ConfigureAwait(false);
-            //assert
-            //retorno.Should().Throws<UnidadeSaudeException>();
 
             //act & assert
             Assert.Throws<UnidadeSaudeException>(async () => await servico.ObterUnidadesSaude(contrato).ConfigureAwait(false));
@@ -88,27 +82,24 @@ namespace Modelo.Ubs.Teste.Integracoes.Servicos
         [Test]
         public void deve_lancar_excecao_se_longitude_for_maior_que_180_graus_positivo_ou_negativo()
         {
-            //arrange
             var contrato = new UnidadeSaudeContratoBuilder()
                 .ComLongitude(181.01)
                 .Construir();
-            //act
+
             Func<Task> retorno = async () => await servico.ObterUnidadesSaude(contrato).ConfigureAwait(false);
-            //assert
+
             retorno.Should().Throws<UnidadeSaudeException>();
         }
 
         //TODO:
         //public async Task handle_deve_obter_as_unidades_basicas_de_saude_ordernada_por_melhor_avaliacao_e_proximo_das_coordenadas_passadas()
         //{
-        //    //arrange
         //    var contrato = new UnidadeSaudeContratoBuilder().Construir();
         //    var comando = new UnidadeSaudeRequisicao(contrato);
         //    var handler = new UnidadeSaudeHandler();
-
-        //    //act
+        //
         //    var retorno = await handler.Handle(comando, new CancellationToken());
-        //    //assert
+        //
         //    retorno.Should().HaveCountLessOrEqualTo(5);
         //    retorno.FirstOrDefault().Nome.Should().NotBeNullOrEmpty();
         //    retorno.FirstOrDefault().Endereco.Should().NotBeNullOrEmpty();
